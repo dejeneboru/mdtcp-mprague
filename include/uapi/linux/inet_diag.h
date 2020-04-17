e24f76d6aa12ea3999f23b844a46fc52b3e9bda2
dboru <dejene.boru@gmail.com>
2020-04-17 10:58:57 +0200

    prague stats diag

--- include/uapi/linux/inet_diag.h
+++ include/uapi/linux/inet_diag.h
@@ -208,8 +208,9 @@ struct tcp_bbr_info {
 };
 /* INET_DIAG_PRAGUEINFO */
 struct tcp_prague_info {
-	__u32	prague_alpha;
-	__u32	prague_max_burst;
+	__u32	prague_alpha;  /* Congestion level EWMA */
+	__u32	prague_max_burst; /* Max GSO burst size */
+        __u8	prague_enabled;	  /* Whether this is prague or reno */
 };
 union tcp_cc_info {
 	struct tcpvegas_info	vegas;
